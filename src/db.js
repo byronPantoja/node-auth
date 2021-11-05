8d2b730934f67765ed749f63bc9c67bd664e3673
“dbp” <byronpantoja@gmail.com>
2021-11-06 05:40:20 +0800

    Set up interface to catalog values from a form

--- src/db.js
+++ src/db.js
@@ -2,18 +2,20 @@ import mongo from "mongodb";
 
 const { MongoClient } = mongo;
 
-const url = process.env.MONGO_URL;
+const url = encodeURI(process.env.MONGO_URL);
 
 export const client = new MongoClient(url, { useNewUrlParser: true });
 
 export async function connectDb() {
   try {
     await client.connect();
-    await client.db("admin").command({ ping: 1 });
 
-    console.log("Connected to db");
+    // Confirm connection
+    await client.db("dev833").command({ ping: 1 });
+    console.log("🗄️ Connected to DB Success");
   } catch (e) {
     console.error(e);
+    // If there is a problem close connection to db
     await client.close();
   }
 }
--- src/index.js
+++ src/index.js
@@ -11,18 +11,10 @@ const __dirname = path.dirname(__filename);
 
 const app = fastify();
 
-console.log(process.env.MONGO_URL);
-
 async function startApp() {
   try {
     app.register(fastifyStatic, {
       root: path.join(__dirname, "public"),
-    });
-
-    app.get("/", {}, (request, reply) => {
-      reply.send({
-        data: "hello world",
-      });
     });
 
     await app.listen(3000);
